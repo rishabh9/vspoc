76594a06da1b08fa78b2de6f18fefeda3123e94e
Rishabh Joshi <rishabh9@gmail.com>
2013-03-27 00:01:25 +0530

    Added a helper class to read properties

---
+++ src/main/java/me/rishabh/vspoc/utilities/PropertiesHelper.java
@@ -0,0 +1,84 @@
+package me.rishabh.vspoc.utilities;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Class to load and cache the properties file
+ * 
+ * @author rishabh
+ * 
+ */
+public class PropertiesHelper {
+
+	private static final Properties prop = new Properties();
+	private static boolean isCached = false;
+
+	private static final SimpleLogger LOG = SimpleLogger
+			.getLogger(PropertiesHelper.class);
+
+	private PropertiesHelper() throws IOException {
+	}
+
+	private static void loadPropertiesFile() {
+		String methodName = "loadPropertiesFile()";
+		LOG.info(methodName, "Reading properties file from classpath...");
+		try {
+			prop.load(PropertiesHelper.class.getClassLoader()
+					.getResourceAsStream("config.properties"));
+			isCached = true;
+			LOG.info(methodName, "Properties file loaded and cached");
+		} catch (IOException e) {
+			LOG.exception(methodName, "Properties file not found in classpath",
+					e);
+		}
+	}
+
+	/**
+	 * Searches for the property with the specified key in this property list.
+	 * If the key is not found in this property list, the default property list,
+	 * and its defaults, recursively, are then checked. The method returns null
+	 * if the property is not found.
+	 * 
+	 * @param key
+	 *            the property key.
+	 * @return the value in this property list with the specified key value.
+	 */
+	public static String getProperty(String key) {
+		return getProperty(key, null);
+	}
+
+	/**
+	 * Searches for the property with the specified key in this property list.
+	 * If the key is not found in this property list, the default property list,
+	 * and its defaults, recursively, are then checked. The method returns the
+	 * default value argument if the property is not found.
+	 * 
+	 * @param key
+	 *            The property key
+	 * @param defaultValue
+	 *            a default value.
+	 * @return the value in this property list with the specified key value.
+	 */
+	public static String getProperty(String key, String defaultValue) {
+		String methodName = "getProperty(key,defaultValue)";
+
+		LOG.info("getProperty()", "getting value for key - " + key);
+		String value;
+		if (!isCached) {
+			LOG.info(methodName, "Properties file wasn't loaded! loading...");
+			loadPropertiesFile();
+		}
+		return prop.getProperty(key, defaultValue);
+	}
+
+	/**
+	 * Method to help in unit testing
+	 * 
+	 * @return TRUE if properties file is cached.
+	 */
+	static boolean isCached() {
+		return isCached;
+	}
+
+}
