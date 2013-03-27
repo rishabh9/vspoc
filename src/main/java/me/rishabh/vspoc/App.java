80a577882d2042469380b143535c914c528303e5
Rishabh Joshi <rishabh9@gmail.com>
2013-03-27 23:24:18 +0530

    modified files to read from BufferedReader directly instead of custom class

--- src/main/java/me/rishabh/vspoc/App.java
+++ src/main/java/me/rishabh/vspoc/App.java
@@ -3,64 +3,60 @@ package me.rishabh.vspoc;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
-
-import me.rishabh.vspoc.reader.InputData;
 
 /**
  * Hello world!
  */
 public class App {
 
-	public static void main(String[] args) {
-		Calendar c = Calendar.getInstance();
-		c.setTimeInMillis(-19800000L);
-		System.out.println(c.getTimeInMillis());
-		System.out.println(c.getTime().toString());
-		App app = new App();
-		BufferedReader reader = null;
-		BufferedWriter writer = null;
-		try {
-			InputData dataReader = new InputData(
-					"/media/personal/Int/code/vspoc/src/main/resources/inputdata.txt");
-			reader = dataReader.getBufferedReader();
-			writer = new BufferedWriter(
-					new FileWriter(
-							"/media/personal/Int/code/vspoc/src/main/resources/output.txt"));
-			String data = reader.readLine();
-			while (data != null) {
-				String ms = app.extractMilliSeconds(data);
-				Calendar cal = Calendar.getInstance();
-				cal.setTimeInMillis(Long.parseLong(ms));
-				if (86400000L <= Long.parseLong(ms)) {
-					System.out.println(cal.getTime().toString());
-				}
-				writer.write(cal.getTime().toString() + "\n");
-				// System.out.println(ms);
-				data = reader.readLine();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (null != reader) {
-					reader.close();
-				}
-				if (null != writer) {
-					writer.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(-19800000L);
+        System.out.println(c.getTimeInMillis());
+        System.out.println(c.getTime().toString());
+        App app = new App();
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "/media/personal/Int/code/vspoc/src/main/resources/inputdata.txt"));
+            writer = new BufferedWriter(new FileWriter("/media/personal/Int/code/vspoc/src/main/resources/output.txt"));
+            String data = reader.readLine();
+            while (data != null) {
+                String ms = app.extractMilliSeconds(data);
+                Calendar cal = Calendar.getInstance();
+                cal.setTimeInMillis(Long.parseLong(ms));
+                if (86400000L <= Long.parseLong(ms)) {
+                    System.out.println(cal.getTime().toString());
+                }
+                writer.write(cal.getTime().toString() + "\n");
+                // System.out.println(ms);
+                data = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != reader) {
+                    reader.close();
+                }
+                if (null != writer) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-		}
-	}
+        }
+    }
 
-	private String extractMilliSeconds(String data) {
-		return data.substring(1);
-	}
+    private String extractMilliSeconds(String data) {
+        return data.substring(1);
+    }
 }
--- src/main/java/me/rishabh/vspoc/CalculateFastSlow.java
+++ src/main/java/me/rishabh/vspoc/CalculateFastSlow.java
@@ -2,47 +2,45 @@ package me.rishabh.vspoc;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-
-import me.rishabh.vspoc.reader.InputData;
 
 public class CalculateFastSlow {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		CalculateFastSlow cfs = new CalculateFastSlow();
-		BufferedReader reader = null;
-		try {
-			InputData dataReader = new InputData(
-					"/media/personal/Int/code/vspoc/src/main/resources/inputdata.txt");
-			reader = dataReader.getBufferedReader();
-			String data = reader.readLine();
-			while (data != null) {
-				if (cfs.isA(data)) {
-					data = reader.readLine();
-					if (cfs.isA(data)) {
-						data = reader.readLine();
-						if (cfs.isB(data)) {
-							System.out.println("Encountered AAB");
-						}
-					}
-				}
-				data = reader.readLine();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        CalculateFastSlow cfs = new CalculateFastSlow();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "/media/personal/Int/code/vspoc/src/main/resources/inputdata.txt"));
+            String data = reader.readLine();
+            while (data != null) {
+                if (cfs.isA(data)) {
+                    data = reader.readLine();
+                    if (cfs.isA(data)) {
+                        data = reader.readLine();
+                        if (cfs.isB(data)) {
+                            System.out.println("Encountered AAB");
+                        }
+                    }
+                }
+                data = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private boolean isB(String data) {
-		return data.startsWith("B");
-	}
+    private boolean isB(String data) {
+        return data.startsWith("B");
+    }
 
-	private boolean isA(String data) {
-		return data.startsWith("A");
-	}
+    private boolean isA(String data) {
+        return data.startsWith("A");
+    }
 }
